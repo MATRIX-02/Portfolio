63fc2bf7c0823d1c0bae166992402badb377ef9c
Mayank <76843683+MATRIX-02@users.noreply.github.com>
2023-11-25 20:39:55 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -7,10 +7,7 @@ const Content = () => {
 			<ProjectCard />
 			<ProjectCard />
 			<ProjectCard />
-			<ProjectCard />
-			<ProjectCard />
-			<ProjectCard />
-			<ProjectCard />
+			
 		</div>
 	);
 };
