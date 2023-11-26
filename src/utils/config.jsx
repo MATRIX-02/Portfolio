7f5756ea205f5260a40ede96ebf24acb151024b1
Mayank <mayanksingh12@gmail.com>
2023-11-26 19:37:20 +0530

    projectImg bug check 2

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,25 +1,25 @@
 const projectList = [
 	{
 		id: 1001,
-		projectImg: "../assets/discord-clone.webp",
+		projectImg: "../../public/Images/discord-clone.webp",
 		projectName: "Project1",
 		projectDesc: "This is a sample project 1",
 	},
 	{
 		id: 1002,
-		projectImg: "../src/assets/discord-clone.webp",
+		projectImg: "../../public/Images/discord-clone.webp",
 		projectName: "Project2",
 		projectDesc: "This is a sample project 2",
 	},
 	{
 		id: 1003,
-		projectImg: "../src/assets/discord-clone.webp",
+		projectImg: "../../public/Images/discord-clone.webp",
 		projectName: "Project3",
 		projectDesc: "This is a sample project 3",
 	},
 	{
 		id: 1004,
-		projectImg: "../src/assets/discord-clone.webp",
+		projectImg: "../../public/Images/discord-clone.webp",
 		projectName: "Project4",
 		projectDesc: "This is a sample project 4",
 	},
